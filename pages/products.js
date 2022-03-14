7a6b719a28231bbcfa6a3bb0ce0b97e2854eeaad
Arslan Zia <ziaarslan13@gmail.com>
2022-03-14 17:45:31 +0500

    final

--- pages/products.js
+++ pages/products.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Grid from "@mui/material/Grid"
 import Card from "../Components/Card"
+import Navbar from '../components/Navbar';
 const Products = () => {
   return (
   <>
+  <Navbar/>
         <div className="main-product">
             <div className="product-container">
             <Grid container spacing={2}>
                 <Grid item xs={4}>
                   <Card/>
                 </Grid>
-                <Grid item xs={4}>
+                {/* <Grid item xs={4}>
                 <Card/>
                 </Grid>
                 <Grid item xs={4}>
@@ -24,7 +26,7 @@ const Products = () => {
                 </Grid>
                 <Grid item xs={4}>
                 <Card/>
-                </Grid>
+                </Grid> */}
                 </Grid>                   
             </div>
         </div>
